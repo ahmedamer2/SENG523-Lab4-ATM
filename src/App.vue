e876988aaca27de67329348326a54260ec4d63fc
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2022-11-27 16:41:11 -0700

    Started Project and Finished Keypad Design

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ATM from "./components/ATM.vue";
+</script>
+
+<template>
+  <body>
+    <header id="header">
+      <div class="title">SENG 523 - ATM Application</div>
+      <div class="subtitle">
+        Ahmed Amer, Ian Kang, Luke Hollinda, Brian Kramer, Deivydas Johnson
+      </div>
+    </header>
+    <main id="main">
+      <ATM></ATM>
+    </main>
+  </body>
+</template>
+
+<style lang="less" scoped>
+#header {
+  padding: 1.2em;
+  text-align: center;
+  font-family: "Barlow-Regular", Arial, Helvetica, sans-serif;
+
+  .title {
+    font-family: "Barlow-Bold";
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+
+  .subtitle {
+    font-family: "Barlow-Medium";
+    font-size: 1.5rem;
+  }
+}
+
+#main {
+  height: calc(100% - 8.9rem);
+}
+</style>
---
+++ src/components/ATM.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="ATM">
+    <div id="screen">screen</div>
+    <div id="bottom">
+      <div id="keypad" class="grid">
+        <div class="button">1</div>
+        <div class="button">2</div>
+        <div class="button">3</div>
+        <div class="button cancel">Cancel</div>
+        <div class="button">4</div>
+        <div class="button">5</div>
+        <div class="button">6</div>
+        <div class="button clear">Clear</div>
+        <div class="button">7</div>
+        <div class="button">8</div>
+        <div class="button">9</div>
+        <div class="button enter">Enter</div>
+        <div class="button zero">0</div>
+      </div>
+      <div id="cardScanner">cardScanner</div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+#ATM {
+  height: 85%;
+  width: 65rem;
+  margin: 0 auto;
+  padding: 2rem;
+  border: 2px solid black;
+  font-family: "Barlow-Regular";
+
+  #screen {
+    height: 45%;
+    border: 2px solid grey;
+    width: 95%;
+    margin: 0 auto 2rem;
+    padding: 1em;
+  }
+
+  #bottom {
+    display: flex;
+    width: 70%;
+    margin: 0 auto;
+    gap: 1em;
+
+    #keypad {
+      font-family: "Barlow-SemiBold";
+      font-size: 1.2rem;
+      width: 65%;
+      padding: 1rem;
+      outline: 1px solid black;
+      text-align: center;
+      &.grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: 1.5rem;
+
+        .button {
+          outline: 1px solid #555;
+          padding: 1rem;
+          box-shadow: 2px 2px 3px 2px #bbb;
+          border-radius: 5px;
+          cursor: pointer;
+
+          &:hover {
+            background: #ccc;
+          }
+
+          &.zero {
+            grid-column: 1/4;
+          }
+
+          &.enter {
+            grid-row: 3/5;
+            grid-column: 4;
+            background: #2bb82b;
+
+            &:hover {
+              background: #44c244;
+            }
+          }
+
+          &.clear {
+            background: #ebeb38;
+            &:hover {
+              background: #e0e030;
+            }
+          }
+
+          &.cancel {
+            background: #ff2b2b;
+            &:hover {
+              background: #e22929;
+            }
+          }
+        }
+      }
+    }
+
+    #cardScanner {
+      width: 30%;
+      outline: 1px solid black;
+      height: 2rem;
+      margin: auto 0;
+      padding: 1em;
+    }
+  }
+}
+</style>
